c3608d757ab65481760f5ce209d01ed51934ef88
Iurii_Young <yura.young2002@gmail.com>
2024-10-06 20:29:15 +0200

    2nd step

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,13 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
+
+# if capitalize('hello') != 'Hello':
+#     raise Exception('Функция работает неверно!')
+
+# if capitalize('') != '':
+#     raise Exception('Функция работает неверно!')
 
 print('Все тесты пройдены!')
